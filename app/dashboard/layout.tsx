f5dfaa4290a8bb84276d38993675a31440f82989
richTheAnalyst <richardkodah2003@gmail.com>
2026-04-24 10:33:06 +0000

    "new commit message"

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,7 +10,9 @@ export default function Layout({
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="grow p-6 md:overflow-y-auto md:p-12">
+        {children}
+        </div>
     </div>
     );
 }
